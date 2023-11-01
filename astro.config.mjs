32ac1e1b5604cf051ae934d704eed18c07db4425
fernandoMendIA <147778083+fernandoMendIA@users.noreply.github.com>
2023-10-31 21:02:49 -0400

    base url added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: 'https://fernandoMendIA.github.io',
-  base: '/final-tecnologias-internet-speed.github.io'
+
 });
