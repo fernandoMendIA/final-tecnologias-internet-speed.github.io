75043b9082d396cfd21367911b896f0b0eed7400
fernandoMendIA <147778083+fernandoMendIA@users.noreply.github.com>
2023-10-31 20:26:07 -0400

    deployment settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  site: 'https://fernandoMendIA.github.io',
+  base: '/final-tecnologias-internet-speed.github.io'
 });
