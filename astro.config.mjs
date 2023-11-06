c37f7bb5e086c7ecaea6c0b427796484e6551dcf
fernandoMendIA <147778083+fernandoMendIA@users.noreply.github.com>
2023-11-05 22:36:10 -0400

    workflow actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: 'https://fernandoMendIA.github.io',
+  site: 'https://fernandoMendIA.github.io', // workflow commit actions
 
 });
